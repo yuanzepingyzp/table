5dd4ddd461d796cec3cc166253819b4f8db3ae13
yuanzeping <yuanzepingyzp@163.com>
2016-06-26 19:55:48 +0800

    json
    get json

--- handle.js
+++ handle.js
@@ -1,44 +1,43 @@
 var app=angular.module('myapp',[]);
-app.controller('myctrl',function($scope,$http,$filter){
-  $http.get("data.json").success(function(response){
-    $scope.stu=response.data;
-      $scope.showstu=$scope.stu.slice(0,6);
+app.controller('myctrl',function($scope,$filter,$http){
+    $http.get("data.json").success(function(response){
+    $scope.stu=response.students;
+    $scope.showstu=$scope.stu.slice(0,9);
   })
-  
   $scope.isreverse=false;
   $scope.orderbyname=function(){
     $scope.stu=$filter('orderBy')($scope.stu,'name',$scope.isreverse);
     $scope.isreverse=!($scope.isreverse);
-    $scope.showstu=$scope.stu.slice(0,6);
+    $scope.showstu=$scope.stu.slice(0,9);
   }
   $scope.orderbyid=function(){
     $scope.stu=$filter('orderBy')($scope.stu,'id',$scope.isreverse);
     $scope.isreverse=!($scope.isreverse);
-    $scope.showstu=$scope.stu.slice(0,6);
+    $scope.showstu=$scope.stu.slice(0,9);
   }
   $scope.orderbymath=function(){
     $scope.stu=$filter('orderBy')($scope.stu,'math',$scope.isreverse);
     $scope.isreverse=!($scope.isreverse);
-    $scope.showstu=$scope.stu.slice(0,6);
+    $scope.showstu=$scope.stu.slice(0,9);
   }
   $scope.orderbychinese=function(){
   $scope.stu=$filter('orderBy')($scope.stu,'chinese',$scope.isreverse);
     $scope.isreverse=!($scope.isreverse);
-    $scope.showstu=$scope.stu.slice(0,6);
+    $scope.showstu=$scope.stu.slice(0,9);
   }
   $scope.orderbyenglish=function(){
     $scope.stu=$filter('orderBy')($scope.stu,'english',$scope.isreverse);
     $scope.isreverse=!($scope.isreverse);
-    $scope.showstu=$scope.stu.slice(0,6);
+    $scope.showstu=$scope.stu.slice(0,9);
   }
  $scope.pagenumber=0;
    $scope.prevarray=function(){
-     $scope.pagenumber=$scope.pagenumber-6;
-     $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+6);
+     $scope.pagenumber=$scope.pagenumber-9;
+     $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+9);
    }
    $scope.nextarray=function(){
-     $scope.pagenumber=$scope.pagenumber+6;
-     $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+6);
+     $scope.pagenumber=$scope.pagenumber+9;
+     $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+9);
    }
 
    $scope.ishide=true;
@@ -53,7 +52,7 @@ app.controller('myctrl',function($scope,$http,$filter){
      }
    };
    $scope.islast=function(){
-     if($scope.pagenumber+6==$scope.stu.length)
+     if($scope.pagenumber+9==$scope.stu.length)
      return true;
      else {
        return false;
@@ -63,7 +62,7 @@ app.controller('myctrl',function($scope,$http,$filter){
       if($scope.key!="")
       $scope.showstu=$filter('filter')($scope.stu,{name:$scope.key});
       else {
-        $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+6);
+        $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+9);
       }
 
    }
@@ -71,7 +70,7 @@ app.controller('myctrl',function($scope,$http,$filter){
       if($scope.key1!="")
       $scope.showstu=$filter('filter')($scope.stu,{id:$scope.key1});
       else {
-        $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+6);
+        $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+9);
       }
 
    }
@@ -79,7 +78,7 @@ app.controller('myctrl',function($scope,$http,$filter){
       if($scope.key2!="")
       $scope.showstu=$filter('filter')($scope.stu,{math:$scope.key2});
       else {
-        $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+6);
+        $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+9);
       }
 
    }
@@ -87,7 +86,7 @@ app.controller('myctrl',function($scope,$http,$filter){
       if($scope.key3!="")
       $scope.showstu=$filter('filter')($scope.stu,{chinese:$scope.key3});
       else {
-        $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+6);
+        $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+9);
       }
 
    }
@@ -95,7 +94,7 @@ app.controller('myctrl',function($scope,$http,$filter){
       if($scope.key4!="")
       $scope.showstu=$filter('filter')($scope.stu,{english:$scope.key4});
       else {
-        $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+6);
+        $scope.showstu=$scope.stu.slice($scope.pagenumber,$scope.pagenumber+9);
       }
    }
    $scope.boxhide=true;
